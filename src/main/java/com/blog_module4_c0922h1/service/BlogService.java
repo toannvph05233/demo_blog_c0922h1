f3e68c4caa2bbc7a322381d2a6db075b1b94c49b
toannvph05233 <johntoan98@gmail.com>
2023-02-01 11:24:39 +0700

    up phan trang, session

--- src/main/java/com/blog_module4_c0922h1/service/BlogService.java
+++ src/main/java/com/blog_module4_c0922h1/service/BlogService.java
@@ -3,6 +3,8 @@ package com.blog_module4_c0922h1.service;
 import com.blog_module4_c0922h1.model.Blog;
 import com.blog_module4_c0922h1.repository.IBlogRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -13,8 +15,8 @@ public class BlogService {
     @Autowired
     IBlogRepo iBlogRepo;
 
-    public List<Blog> getAll(){
-        return (List<Blog>) iBlogRepo.findAll();
+    public Page<Blog> getAll(Pageable pageable){
+        return iBlogRepo.findAll(pageable);
     }
 
     public void save(Blog blog){
